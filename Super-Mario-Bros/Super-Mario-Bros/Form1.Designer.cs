a342b6f30daf9d365c9a8347cf7be3325c4c39ee
Dev <Dev@DEV-PC>
2017-05-31 14:41:55 -0400

    :D

--- Super-Mario-Bros/Super-Mario-Bros/Form1.Designer.cs
+++ Super-Mario-Bros/Super-Mario-Bros/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 491);
+            this.ClientSize = new System.Drawing.Size(824, 622);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
