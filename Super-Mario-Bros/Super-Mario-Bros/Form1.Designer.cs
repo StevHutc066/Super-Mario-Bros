1da335c2113382660cecdde97893eb9b19fa143e
logaashb <logaashb222@ed.amdsb.ca>
2017-06-05 14:36:23 -0400

    Commited

--- Super-Mario-Bros/Super-Mario-Bros/Form1.Designer.cs
+++ Super-Mario-Bros/Super-Mario-Bros/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.ClientSize = new System.Drawing.Size(1000, 600);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
